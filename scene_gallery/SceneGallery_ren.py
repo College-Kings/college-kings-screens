0194ff3567584d1d18c674e3d2f466b728aee5d4
Oscar Six <26691547+KiloOscarSix@users.noreply.github.com>
2025-01-18 08:13:06 +0000

    Refactor SceneGallery class to rename _image to idle_image and simplify locked_image property logic

--- scene_gallery/SceneGallery_ren.py
+++ scene_gallery/SceneGallery_ren.py
@@ -13,7 +13,7 @@ init python:
 @dataclass
 class SceneGallery:
     _title: str
-    _image: str
+    idle_image: str
     label: str
     scope: dict[str, object] = field(default_factory=dict)
 
@@ -22,26 +22,12 @@ class SceneGallery:
         return self._title.upper()
 
     @property
-    def idle_image(self) -> "Displayable":
-        try:
-            return self._idle_image
-        except AttributeError:
-            self.idle_image = Transform(self._image, size=(362, 230), pos=(6, 16))
-            return self._idle_image
-
-    @idle_image.setter
-    def idle_image(self, value: "Displayable") -> None:
-        self._idle_image: Displayable = value
-
-    @property
     def locked_image(self) -> "Displayable":
         try:
             return self._locked_image
         except AttributeError:
-            self.locked_image = Transform(
-                self._image, blur=50, size=(362, 230), pos=(6, 16)
-            )
-            return self._locked_image
+            self.locked_image = Transform(self.idle_image, blur=50)
+        return self._locked_image
 
     @locked_image.setter
     def locked_image(self, value: "Displayable") -> None:
